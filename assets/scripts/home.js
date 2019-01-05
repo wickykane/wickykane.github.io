724c762567004ce1c5a5eed3a9e9d4d0ca7b1d15
Quang Huy <wickykane@gmail.com>
2019-01-05 16:31:22 +0700

    update

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,3 +1,5 @@
+var prllxSkillContainer;
+
 $(document).ready(function() {
   // Scroll To Top Default
   $("body,html").animate(
@@ -7,6 +9,46 @@ $(document).ready(function() {
     800
   );
 
+  function initHome() {
+    prllxSkillContainer = TweenLite.to(".skills img", 1, { yPercent: 50, ease: Linear.easeNone, paused: true });
+    TweenLite.ticker.addEventListener("tick", onScroll);
+
+    function onScroll() {
+      scrollTop = $(window).scrollTop();
+      if (scrollTop > 10 && $("body").hasClass("show-hello")) {
+        $("body").removeClass("show-hello");
+      } else if (scrollTop <= 10 && !$("body").hasClass("show-hello")) {
+        $("body").addClass("show-hello");
+      }
+  
+      if (
+        scrollTop > $(".objective").outerHeight() &&
+        scrollTop < $(".objective").outerHeight() + wH
+      ) {
+        TweenLite.to($(".hello"), 0, {
+          y: -(scrollTop - $(".objective").outerHeight()) * 0.5,
+          ease: Linear.easeNone
+        });
+        TweenLite.to($(".objective"), 0, {
+          y: -(scrollTop - $(".objective").outerHeight()) * 0.5,
+          ease: Linear.easeNone
+        });
+      } else {
+        TweenLite.to($(".hello"), 0, { y: 0, ease: Linear.easeNone });
+        TweenLite.to($(".objective"), 0, { y: 0, ease: Linear.easeNone });
+      }
+  
+      var minSkill = $(".skills").offset().top;
+      var maxSkill = $(".skills").offset().top + $(".skills").outerHeight();
+      var normSkill = clamp(
+        normalize(window.pageYOffset, minSkill, maxSkill),
+        0,
+        1
+      );
+      prllxSkillContainer.progress(normSkill);
+    }
+  
+  }
   function normalize(value, min, max) {
     return (value - min) / (max - min);
   }
@@ -31,57 +73,17 @@ $(document).ready(function() {
     wW = $(window).width();
     wH = $(window).height();
   });
+
+
   $(window).trigger("resize");
 
-  TweenLite.ticker.addEventListener("tick", onScroll);
-
-  function onScroll() {
-    scrollTop = $(window).scrollTop();
-    if (scrollTop > 10 && $("body").hasClass("show-hello")) {
-      $("body").removeClass("show-hello");
-    } else if (scrollTop <= 10 && !$("body").hasClass("show-hello")) {
-      $("body").addClass("show-hello");
-    }
-
-    //PRLLX VIDEOCONTAINER
-    var prllxSkillContainer = TweenLite.to(".skills img", 1, {
-      yPercent: 0,
-      ease: Linear.easeNone,
-      paused: true
-    });
-
-    if (
-      scrollTop > $(".objective").outerHeight() &&
-      scrollTop < $(".objective").outerHeight() + wH
-    ) {
-      TweenLite.to($(".hello"), 0, {
-        y: -(scrollTop - $(".objective").outerHeight()) * 0.5,
-        ease: Linear.easeNone
-      });
-      TweenLite.to($(".objective"), 0, {
-        y: -(scrollTop - $(".objective").outerHeight()) * 0.5,
-        ease: Linear.easeNone
-      });
-    } else {
-      TweenLite.to($(".hello"), 0, { y: 0, ease: Linear.easeNone });
-      TweenLite.to($(".objective"), 0, { y: 0, ease: Linear.easeNone });
-    }
-
-    var minSkill = $(".skills").offset().top;
-    var maxSkill = $(".skills").offset().top + $(".skills").outerHeight();
-    var normSkill = clamp(
-      normalize(window.pageYOffset, minSkill, maxSkill),
-      0,
-      1
-    );
-    prllxSkillContainer.progress(normSkill);
-  }
-
+  
   // Parrallax
   var objective = $(".objective.scence")[0];
   new Parallax(objective, {
-    relativeInput: true,
-    hoverOnly: true
+    // relativeInput: true,
+    hoverOnly: true,
+    clipRelativeInput: true,
   });
 
   // Hover Menu
@@ -100,5 +102,6 @@ $(document).ready(function() {
   // Remove Loading
   setTimeout(function() {
     $(".show-loading").toggleClass("show-loading");
+    initHome();
   }, 500);
 });
